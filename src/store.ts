8abfa88a7cd9589f771bb18716a787095000a9a8
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-08 19:57:56 -0300

    Agregado el retorno del valor de la api y seteandolo en el state

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,18 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import { Cryptocurrency, Pair } from "./types"
+import { Cryptocurrency, CryptoPrice, Pair } from "./types"
 import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService"
 
 type CryptoStore = {
     cryptocurrencies: Cryptocurrency[]
+    result: CryptoPrice
     fetchCryptos: () => Promise<void>
     fetchData: (pair : Pair) => Promise<void>
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
     cryptocurrencies : [],
+    result: {} as CryptoPrice,
     fetchCryptos: async () => {
         const cryptocurrencies = await getCryptos()
         set(() => ({
@@ -18,6 +20,9 @@ export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
         }))
     },
     fetchData: async (pair) => {
-        await fetchCurrentCryptoPrice(pair)
+        const result = await fetchCurrentCryptoPrice(pair)
+        set(() => ({
+            result
+        }))
     }
 })))
