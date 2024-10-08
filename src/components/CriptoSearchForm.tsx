549a3a79605d742f2b4bbcd22cffd3dfb7bab2e5
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-08 18:46:29 -0300

    Agregando el state y el handlechange local del form

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,23 @@
+import { useCryptoStore } from "../store"
 import { currencies } from "../data"
+import { ChangeEvent, useState } from "react"
+import { Pair } from "../types"
 
 export default function CriptoSearchForm() {
+
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+    const [pair, setPair] = useState<Pair>({
+        currency: "",
+        cryptocurrency: ""
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [e.target.name]: e.target.value
+        })
+    }
+
   return (
     <form className='form'>
         <div className="field">
@@ -8,6 +25,7 @@ export default function CriptoSearchForm() {
             <select 
                 name="currency" 
                 id="currency"
+                onChange={handleChange}
             >
                 <option value="">-- Seleccione --</option>
                 {currencies.map( currency => (
@@ -17,12 +35,21 @@ export default function CriptoSearchForm() {
         </div>
 
         <div className="field">
-            <label htmlFor="criptocurrency">Criptomoneda</label>
+            <label htmlFor="cryptocurrency">Criptomoneda</label>
             <select 
-                name="criptocurrency" 
-                id="criptocurrency"
+                name="cryptocurrency" 
+                id="cryptocurrency"
+                onChange={handleChange}
             >
                 <option value="">-- Seleccione --</option>
+                {cryptocurrencies.map( crypto => (
+                    <option 
+                        key={crypto.CoinInfo.Name} 
+                        value={crypto.CoinInfo.Name}
+                    >
+                        {crypto.CoinInfo.FullName}
+                    </option>
+                ))}
             </select>
         </div>
 
