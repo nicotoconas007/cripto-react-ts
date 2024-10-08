20abb846e18cb647f99b3f446a10f05f79949d9b
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-08 15:46:53 -0300

    Agregado componente para el form de las monedas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import CriptoSearchForm from "./components/CriptoSearchForm"
 
 function App() {
 
   return (
     <>
-      <h1>Cripto React</h1>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+        <div className="content">
+          <CriptoSearchForm />
+        </div>
+      </div>
     </>
   )
 }
